e1de2e042dfcf33c2597fa8eee644b2ee6d06b44
Philipp Bock <p@philippbock.de>
2014-10-29 14:50:44 +0100

    Added README, modified thumbnails script

--- process-images.sh
+++ process-images.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 cd app/images/
 mkdir thumbs
-mkdir reader
 rm thumbs/*
-rm reader/*
 n=0
-for file in originals/*; do
+for file in reader/*; do
 	n=$((n+1))
 	echo "$file"
 	convert $file -resize 200x -sharpen 1 -quality 70 "thumbs/${n}.jpg" || break
-	convert $file -resize 800x -sharpen 1 -quality 70 "reader/${n}.jpg" || break
 done
