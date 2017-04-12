04f39b29aae1f239bc9c42c03faed1c329075247
Kailyn Nguyen <kailyn@Administrators-MacBook-Pro-2.local>
2017-04-12 12:12:20 +0200

    gh-pages

--- make/thumbnails.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd app/images/
-rm -r thumbs/
-mkdir thumbs
-for file in reader/*; do
-	basename $file
-	convert $file -resize 200x -sharpen 1 -quality 70 "thumbs/`basename $file`" || break
-done
